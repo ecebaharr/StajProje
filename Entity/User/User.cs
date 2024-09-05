b4c8638ff18b4a49e0613f68593499d04653f5b4
Ece Bahar <e_b_ecebahar@hotmail.com>
2024-09-05 12:26:31 +0300

    Add project files.

---
+++ Entity/User/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.User
+{
+    public class User:IdentityUser<int>
+    {
+        
+     
+        public string Surname { get; set; }
+        public DateTime dateTime { get; set; }
+
+    }
+}
